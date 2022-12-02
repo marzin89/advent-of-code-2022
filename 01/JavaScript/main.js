35167e78a01e8ab20bb1d47d15ddda4917704832
marco <mazi2001@student.miun.se>
2022-12-02 01:14:09 +0100

    Add the JavaScript solution to the first puzzle

---
+++ 01/JavaScript/main.js
@@ -0,0 +1,35 @@
+const calorieArr = [];
+const fs = require('fs');
+fs.readFile('input.txt', 'utf8', (err, data) => {
+    if (err) return;
+
+    let inputArr = data.split('\r\n');
+
+    inputArr.forEach((item, index) => {
+        if (!index) {
+            countCalories(inputArr.slice(0, inputArr.indexOf('')));
+
+        } else if (item == '') {
+            countCalories(inputArr.slice((index + 1), inputArr.indexOf('', (index + 1))));
+        }
+    });
+
+    let elf = calorieArr.indexOf(Math.max.apply(null, calorieArr));
+    let calories = calorieArr[elf];
+    console.log(`Elf ${elf + 1} is carrying ${calories} calories`);
+});
+
+function countCalories(itemArr) {
+    let totalCalories;
+
+    itemArr.forEach((item) => {
+        if (!totalCalories) {
+            totalCalories = Number(item);
+        
+        } else {
+            totalCalories += Number(item);
+        }
+    })
+
+    calorieArr.push(totalCalories);
+}
